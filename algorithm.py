54556bed223b5fde0d454188ab759c93c6894299
viswaraavi <viswaraavi@yahoo.in>
2016-10-04 14:05:22 -0400

    intial stub

---
+++ algorithm.py
@@ -0,0 +1,76 @@
+
+
+"""
+should take input files and should return graph in form
+dict{v1:set(adjacency_list),......}
+"""
+def load_graph():
+    pass
+
+"""
+Returns a set of vertices
+"""
+
+def set_vertices():
+    pass
+
+"""
+Returns the degree of vertices as dictionary
+"""
+
+def degree():
+    pass
+
+"""
+Partition the vertices into p
+"""
+
+def partition():
+    pass
+
+"""
+takes input as graph
+select min degree vertex
+"""
+
+def select_vertex_min_degree():
+    pass
+
+
+def calculate_delta_qv():
+    pass
+
+
+
+"""
+compute v
+v = arg max v 0 4Q uv 0 ;
+"""
+
+def compute_v():
+    pass
+
+
+"""
+main function that would be called
+"""
+
+def algorithm():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
