4772f5cc3e28abe7791b29a785f0a8b9b8c57783
viswaraavi <viswaraavi@yahoo.in>
2016-10-04 22:25:21 -0400

    Main algorithm template

--- algorithm.py
+++ algorithm.py
@@ -4,6 +4,9 @@
 should take input files and should return graph in form
 dict{v1:set(adjacency_list),......}
 """
+
+graph={}
+
 def load_graph():
     pass
 
@@ -55,8 +58,48 @@ def compute_v():
 main function that would be called
 """
 
+
+
+
 def algorithm():
-    pass
+    #T is set of vertices
+    T=set_vertices()
+    while(T):
+        #partition function calculates all the nodes in graph with degree less than 0 or equal to 1
+        p=partition()
+        for element in p:
+            new_vertex=graph[element][0]
+            edge_list=graph[new_vertex]
+            edge_list.remove(element)
+            T=T-{element}
+            graph[element+new_vertex]=edge_list
+            del graph[element]
+            del graph[new_vertex]
+
+        u=select_vertex_min_degree()
+        v=compute_v(u)
+        if(calculate_delta_qv(u,v)>0):
+            graph[u+v]=graph[u].remove(v)+graph[v].remove(u)
+            del graph[u]
+            del graph[v]
+            T=T-{u,v}
+            T=T+{u+v}
+        else:
+            T=T-{u}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
